13a0302e04452bb426b81acce9dd056f3508fd9d
Tommaso Checchi <tommaso.checchi1@gmail.com>
2018-06-09 20:33:05 -0700

    lots of random stuff, loading ROMs from file, not that it's useful right now

--- src/cpu.rs
+++ src/cpu.rs
@@ -21,10 +21,29 @@ const HORIZONTAL_SYNC_HZ: u64 = 9198000;
 #[allow(unused)]
 const VERTICAL_SYNC_HZ: f32 = 59.73;
 
+#[allow(unused)]
+const VERTICAL_BLANK_INTERRUPT_START_ADDRESS: u16 = 0x40;
+#[allow(unused)]
+const LCDC_STATUS_INTERRUPT_START_ADDRESS: u16 = 0x48;
+#[allow(unused)]
+const SERIAL_TRANSFER_COMPLETION_INTERRUPT_START_ADDRESS: u16 = 0x58;
+#[allow(unused)]
+const HIGH_TO_LOW_P10_P13_INTERRUPT_START_ADDRESS: u16 = 0x60;
+#[allow(unused)]
+const COLOR_GB_ENABLE_ADDRESS: u16 = 0x143;
+#[allow(unused)]
+const SUPER_GB_ENABLE_ADDRESS: u16 = 0x146;
+const CARTRIDGE_TYPE_ADDRESS: u16 = 0x147;
+#[allow(unused)]
+const ROM_SIZE_ADDRESS: u16 = 0x148;
+#[allow(unused)]
+const RAM_SIZE_ADDRESS: u16 = 0x149;
+const INTERNAL_ROM_TURN_OFF_ADDRESS: u16 = 0xFF50;
+
 //derived data
 const TICK_DURATION: Duration = Duration::from_nanos(1000000000 / MACHINE_HZ);
 
-//TODO move to screen
+//TODO move to CPU/screen
 #[allow(unused)]
 const RESOLUTION_W: u32 = 160;
 #[allow(unused)]
@@ -43,10 +62,10 @@ const MIN_SPRITE_SIZE_W: u32 = 8;
 const MIN_SPRITE_SIZE_H: u32 = 8;
 
 #[allow(unused)]
-const SOUND_CHANNELS:u32 = 4;
+const SOUND_CHANNELS: u32 = 4;
 
 #[allow(non_snake_case)]
-pub struct CPU {
+pub struct CPU<'a> {
     pub PC: u16,
     pub SP: u16,
     pub AF: Register,
@@ -58,11 +77,12 @@ pub struct CPU {
 
     pub cb_mode: bool,
 
-    pub next_clock_time: Instant,
+    next_clock_time: Instant,
+    cartridge_ROM: &'a [u8],
 }
 
-impl CPU {
-    pub fn new() -> Self {
+impl<'a> CPU<'a> {
+    pub fn new(rom: &'a [u8]) -> CPU<'a> {
         let mut cpu = CPU {
             PC: 0,
             SP: 0,
@@ -72,8 +92,16 @@ impl CPU {
             HL: Register { r16: 0 },
             RAM: [0; RAM_SIZE],
             cb_mode: false,
+
             next_clock_time: Instant::now(),
+            cartridge_ROM: rom,
         };
+
+        //setup stuff
+        assert!(
+            rom[CARTRIDGE_TYPE_ADDRESS as usize] == 0,
+            "Not a ROM-Only ROM, not supported"
+        );
 
         // try to load the "bios" rom into the start of the buffer
         File::open("DMG_ROM.bin")
@@ -84,7 +112,7 @@ impl CPU {
         cpu
     }
 
-    pub fn run(&mut self, _rom: &[u8]) {
+    pub fn run(&mut self) {
         loop {
             unsafe {
                 if self.cb_mode {
@@ -130,6 +158,33 @@ impl CPU {
     }
 
     pub fn address(&self, addr: u16) -> u8 {
+        //          Interrupt Enable Register
+        //          --------------------------- FFFF
+        //          Internal RAM
+        //          --------------------------- FF80
+        //          Empty but unusable for I/O
+        //          --------------------------- FF4C
+        //          I/O ports
+        //          --------------------------- FF00
+        //          Empty but unusable for I/O
+        //          --------------------------- FEA0
+        //          Sprite Attrib Memory (OAM)
+        //          --------------------------- FE00
+        //          Echo of 8kB Internal RAM
+        //          --------------------------- E000
+        //          8kB Internal RAM
+        //          --------------------------- C000
+        //          8kB switchable RAM bank
+        //          --------------------------- A000
+        //          8kB Video RAM
+        //          --------------------------- 8000 -
+        //          16kB switchable ROM bank         |
+        //          --------------------------- 4000 |= 32kB Cartrige
+        //          16kB ROM bank #0                 |
+        //          --------------------------- 0000 -
+
+        //    * NOTE: b = bit, B = byte
+
         //TODO the address space and interrupts are a lot more complex than that...
         self.RAM[addr as usize]
     }
@@ -142,6 +197,11 @@ impl CPU {
     }
 
     pub fn set_address(&mut self, addr: u16, val: u8) {
+        if addr == INTERNAL_ROM_TURN_OFF_ADDRESS && val == 1{
+            //load the cartridge rom
+            self.RAM.copy_from_slice(self.cartridge_ROM);
+        }
+
         self.RAM[addr as usize] = val;
     }
 
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,51 @@
 extern crate bit_field;
+extern crate clap;
 
 mod cpu;
-mod interpreter;
 mod function_stubs;
+mod interpreter;
 
+use std::fs::File;
+use clap::{App, Arg};
 use cpu::CPU;
+use std::io::Read;
+use std::path::Path;
+
+fn open_rom<P: AsRef<Path>>(rom_path: &P) -> std::io::Result<Vec<u8>> {
+    let mut content = vec![];
+    File::open(rom_path)?.read_to_end(&mut content)?;
+
+    //TODO unzip?
+
+    Ok(content)
+}
 
 fn main() {
-    let mut cpu = CPU::new();
+    const VERSION: &'static str = env!("CARGO_PKG_VERSION");
 
-    let rom = &[0 as u8; 1];
-    cpu.run(rom);
+    let matches = App::new("GAMEBOII")
+        .version(VERSION)
+        .about("It plays the gameboy dance")
+        .arg(
+            Arg::with_name("ROMFILE")
+                .value_name("FILE")
+                .help("Set the cartridge ROM to load")
+                .takes_value(true)
+                .required(true)
+        )
+        .get_matches();
+
+    //load the file from command line
+    let rom_path = matches.value_of("ROMFILE").unwrap();
+    
+    let rom = open_rom(&rom_path).unwrap_or_else(|error| {
+        println!("Cannot open file: {}", rom_path);
+        println!("An error occurred:");
+        println!("{}", error);
+        std::process::exit(1);
+    });
+    
+    let mut cpu = CPU::new(&rom);
+
+    cpu.run();
 }
